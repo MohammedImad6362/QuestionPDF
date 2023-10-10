4cd024615a13e8b0f7a01a8b895c3ea24dbdeb50
MohammedImad6362 <mdimad@futuretechschool.in>
2023-10-10 18:00:49 +0530

    updated ejs file

---
+++ data.js
@@ -0,0 +1,64 @@
+const questionData = [
+	{
+		"_id": {
+		  "$oid": "5e04448f9cd3660511647189"
+		},
+		"matrixSize": {
+		  "rows": 0,
+		  "columns": 0
+		},
+		"tags": [
+		  "English"
+		],
+		"difficulty": "2",
+		"assets": [],
+		"type": "mcq",
+		"partialMarking": false,
+		"hasRelatedQuestions": false,
+		"childQuestions": [],
+		"question": {
+		  "text": "<img src='https://testimages.futuretechschool.in/5e049844efa4c57e9eb6b07a.jpg' />"
+		},
+		"options": [
+		  {
+			"d": {
+			  "text": "1,2 and 4"
+			},
+			"v": 0
+		  },
+		  {
+			"d": {
+			  "text": "1,2 and 3"
+			},
+			"v": 1
+		  },
+		  {
+			"d": {
+			  "text": "1,2,3 and 4"
+			},
+			"v": 2
+		  },
+		  {
+			"d": {
+			  "text": "2. 3 and 4"
+			},
+			"v": 3
+		  }
+		],
+		"answer": 1,
+		"solution": {
+		  "text": "The correct matched pairs are:Kshetra — Cultivated land Khila — Waste land Aprahata Forest landThese are the types of land available in the country at that time. Despite these, there is one more class called Charagah land, which is used as a Pasteur land"
+		},
+		"comprehension": "",
+		"topicId": {
+		  "$oid": "5de8f208f2109b7c7646f699"
+		},
+		"subTopicId": {
+		  "$oid": "5de8f208f2109b7c7646f69a"
+		},
+		"__v": 0,
+		"disabled": true
+	  }
+]
+
+module.exports = questionData;
--- index2.js
+++ index2.js
@@ -1,43 +1,47 @@
 const puppeteer = require('puppeteer');
 const ejs = require('ejs');
 const fs = require('fs');
-const path = require('path')
-const questionData = require('./UPSC-JSON/Subject-9/merged-questions-chunk1.json')
 
-console.log(questionData)
+const questionData = require('./dataNewExample.json'); // Load your JSON data (remove the .js extension)
 const htmlTemplate = fs.readFileSync('template.ejs', 'utf8'); // Read the HTML template file
-
+let index = 1;
 (async () => {
-    const browser = await puppeteer.launch({
-        protocolTimeout: 4.32e+7, // Increase the timeout to 30 seconds (or adjust as needed)
-    });
+	const browser = await puppeteer.launch();
+	const page = await browser.newPage();
 
-    const page = await browser.newPage();
+	// Create an array to store HTML content for each question
+	const htmlContents = [];
 
-    // Create an array to store HTML content for each question
-    const htmlContents = [];
-    // console.log(questionData);
+	// for (let item of questionData) {
+	// 	// Compile the EJS template with the JSON data for each question
+	// 	const compiledTemplate = ejs.compile(htmlTemplate);
+	// 	const htmlContent = compiledTemplate({
+	// 		data: item,
+	// 		index,
+	// 	});
 
-    for (let index = 0; index < questionData.length; index++) {
-        const item = questionData[index];
-        // Compile the EJS template with the JSON data for each question
-        const compiledTemplate = ejs.compile(htmlTemplate);
-        const htmlContent = compiledTemplate({
-            data: item,
-            index: index + 1,
-        });
+	// 	// Push the HTML content into the array
+	// 	htmlContents.push(htmlContent);
+	// 	index++;
+	// }
+	questionData.forEach(async (item, index) => {
+		// Compile the EJS template with the JSON data for each question
+		const compiledTemplate = ejs.compile(htmlTemplate);
+		const htmlContent = compiledTemplate({
+			data: item,
+			index: index + 1,
+		});
+		// Push the HTML content into the array
+		htmlContents.push(htmlContent);
+		// index++;
+	});
 
-        // Push the HTML content into the array
-        htmlContents.push(htmlContent);
-    }
+	// Set the HTML content of the page with all questions
+	await page.setContent(htmlContents.join('')); // Join all HTML contents into a single string
 
-    // Set the HTML content of the page with all questions
-    await page.setContent(htmlContents.join('')); // Join all HTML contents into a single string
+	// Generate a single PDF containing all questions
+	await page.pdf({ path: 'all_questions.pdf', format: 'A4' });
 
-    // Generate a single PDF containing all questions
-    const pdfPath = path.join(__dirname,'PDF','upsc','9-Current Affairs','Part1.pdf')
-    await page.pdf({ path: pdfPath, format: 'A4' });
-
-    await browser.close();
-    console.log('PDF with all questions generated successfully.');
+	await browser.close();
+	console.log('PDF with all questions generated successfully.');
 })();
